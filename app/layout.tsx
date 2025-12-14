c5d9172753c97da05cb67473a21bd69dce096ef3
Angelo <angelowhite871@gmail.com>
2025-12-13 20:18:42 -0500

    added more stuff to the homePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <body>
           <header className="bg-linear-to-b from-black-900 to-gray-900 text-white py-6 pr-6 flex">
             <Link href="/"><h1 className="text-4xl pl-[10%] pr-[50%] hover:opacity-80 transition">LOGO</h1></Link>
-            <div className="flex gap-6 ml-auto pt-2">
+            <div className="headingFont flex gap-6 ml-auto pt-2">
               <Link href="/"><h2 className="font-bold hover:opacity-40 transition">Home</h2></Link>
               <Link href="/"><h2 className="font-bold hover:opacity-40 transition">Gallery</h2></Link>
               <Link href="/"><h2 className="font-bold hover:opacity-40 transition">Animations</h2></Link>
